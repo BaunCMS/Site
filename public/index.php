de08c2533d850a49ddb44cc8bfd32d9d3b5235d3
Gilbert Pellegrom <gilbert@pellegrom.me>
2015-03-10 16:03:13 +0000

    Update Baun

--- public/index.php
+++ public/index.php
@@ -2,7 +2,15 @@
 
 define('BASE_PATH', dirname(__DIR__) . '/');
 
-require BASE_PATH . 'vendor/autoload.php';
+if (file_exists(BASE_PATH . 'vendor/autoload.php')) {
+	require BASE_PATH . 'vendor/autoload.php';
 
-$baun = new Baun\Baun();
-$baun->run();
+	$baun = new Baun\Baun();
+	$baun->run();
+} else {
+	if (file_exists(BASE_PATH . 'auto-install.php')) {
+		require BASE_PATH . 'auto-install.php';
+	} else {
+		die('Missing vendor/autoload.php. Run <code>composer install</code>.');
+	}
+}
